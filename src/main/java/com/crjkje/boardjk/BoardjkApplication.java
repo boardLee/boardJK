16c6b8748196bb75161f975626d087c2aa8ee61a
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-19 00:47:15 +0900

    init : project 생성

---
+++ src/main/java/com/crjkje/boardjk/BoardjkApplication.java
@@ -0,0 +1,13 @@
+package com.crjkje.boardjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardjkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardjkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crjkje/boardjk/BoardjkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crjkje.boardjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardjkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
